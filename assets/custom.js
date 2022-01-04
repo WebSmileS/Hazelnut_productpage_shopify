a060b972ad5b4f4d8de3053756c4b4b90639209e
Harley990220 <95194939+Harley990220@users.noreply.github.com>
2022-01-04 16:17:56 +0900

    last commit

---
+++ assets/custom.js
@@ -0,0 +1,11 @@
+
+// slick
+$(document).ready(function () {
+    $('.slick1').slick({
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});
